a20fc8c712133ecb92075db59250c83cd90f0e67
YugandharDachepalli <yugandhardachepalli999@gmail.com>
2022-09-20 14:08:47 +0530

    New1

--- Yuga.java
+++ Yuga.java
@@ -2,6 +2,9 @@ class Yuga
 {
   public static void main(String[]args)
   {
-    System.out.println("Hello Worls");
+    System.out.println("Hello World");
+    System.out.println("Hello ");
+    System.out.println("Hello World");
+    System.out.println("Hello ");
   }
 }
